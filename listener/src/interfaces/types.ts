4ec38ab658a465ada5f62474daa6f00977116b2a
aantoniorodrigues <antoniocvpr@gmail.com>
2025-02-28 15:51:09 +0800

    Add constants and define types

--- listener/src/interfaces/types.ts
+++ listener/src/interfaces/types.ts
@@ -1,1 +1,13 @@
-// Define types
+export interface PriceData {
+  exchange: string;
+  price: number;
+  timestamp: number;
+}
+
+export interface PriceDifference {
+  uniswapPrice: number;
+  sushiswapPrice: number;
+  difference: number;
+  percentageDifference: number;
+  timestamp: number;
+}
