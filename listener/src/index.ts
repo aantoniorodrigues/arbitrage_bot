864ed07fe05b6ddc42e4ed50f959095ed2cd129a
aantoniorodrigues <antoniocvpr@gmail.com>
2025-05-08 19:38:20 +0800

    Add function that runs periodically

--- listener/src/index.ts
+++ listener/src/index.ts
@@ -1,1 +1,9 @@
-// Actual listener of the DEXs
+import { POLLING_INTERVAL } from "./constants";
+
+setInterval(() => {
+  try {
+    console.log("Run price difference logic.");
+  } catch (error) {
+    console.error("Error when checking price differences:", error);
+  }
+}, POLLING_INTERVAL);
