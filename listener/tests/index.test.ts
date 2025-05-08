b15440789740a87d2cc7ebef6889f77feaf3de4f
aantoniorodrigues <antoniocvpr@gmail.com>
2025-05-08 20:32:21 +0800

    Test configs using Jest

---
+++ listener/tests/index.test.ts
@@ -0,0 +1,10 @@
+// Used as an example to make sure the tests were configured and running correctly
+
+describe("computePriceDifference", () => {
+  it("should be called periodically", () => {
+    // TODO: Implement actual test
+    const result = 3;
+    const expected = 3;
+    expect(result).toBe(expected);
+  });
+});
