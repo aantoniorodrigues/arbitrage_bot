78b7c653a79d6d975cc4500a42d6e95746b9e1c4
aantoniorodrigues <antoniocvpr@gmail.com>
2025-06-26 13:27:03 +0100

    fix create pair function

--- listener/src/dexs/uniswap.ts
+++ listener/src/dexs/uniswap.ts
@@ -4,13 +4,12 @@ import { Pair } from "@uniswap/v2-sdk";
 import { ethers } from "ethers";
 import uniswapV2poolABI from "@uniswap/v2-core/build/UniswapV2Pair.json";
 
-const chainId = ChainId.MAINNET;
-const provider = new ethers.JsonRpcProvider(process.env.ETHEREUM_RPC_URL);
-const WETH = new Token(chainId, TOKENS.WETH.mainnet, TOKENS.WETH.decimals);
-const DAI = new Token(chainId, TOKENS.DAI.mainnet, TOKENS.DAI.decimals);
-
-async function createPair(token1: Token, token2: Token) {
-  const pairAddress = Pair.getAddress(token1, token2);
+export async function createPair(
+  token0: Token,
+  token1: Token,
+  provider: ethers.JsonRpcProvider
+): Promise<Pair> {
+  const pairAddress = Pair.getAddress(token0, token1);
   const pairContract = new ethers.Contract(
     pairAddress,
     uniswapV2poolABI.abi,
@@ -18,15 +17,20 @@ async function createPair(token1: Token, token2: Token) {
   );
 
   const reserves = await pairContract["getReserves"]();
-  const [reserve1, reserve2] = reserves;
+  const [reserve0, reserve1] = reserves;
 
-  const tokensSorted = token1.sortsBefore(token2)
-    ? [token1, token2]
-    : [token2, token1];
+  const tokens = [token0, token1];
+  const tokensSorted = tokens[0].sortsBefore(tokens[1])
+    ? tokens
+    : [tokens[1], tokens[0]];
+
+  const reservesSorted = token0.sortsBefore(token1)
+    ? [reserve0, reserve1]
+    : [reserve1, reserve0];
 
   const pair = new Pair(
-    CurrencyAmount.fromRawAmount(token1, reserve1),
-    CurrencyAmount.fromRawAmount(token2, reserve2)
+    CurrencyAmount.fromRawAmount(tokensSorted[0], reservesSorted[0].toString()),
+    CurrencyAmount.fromRawAmount(tokensSorted[1], reservesSorted[1].toString())
   );
 
   return pair;
