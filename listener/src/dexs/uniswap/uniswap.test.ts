75c095d252a8630ada62efca06493592bfcc0e5e
Antonio Rodrigues <antoniocvpr@gmail.com>
2025-10-29 16:46:42 +0000

    directory for uniswap logic

---
+++ listener/src/dexs/uniswap/uniswap.test.ts
@@ -0,0 +1,75 @@
+import { TOKENS } from "../../constants";
+import { ChainId, Token } from "@uniswap/sdk-core";
+import { createPair, getMidPrice, getExecutionPrice } from "./uniswap";
+import { ethers } from "ethers";
+
+let USDT: Token;
+let WETH: Token;
+let BNB: Token;
+let provider: ethers.JsonRpcProvider;
+
+beforeEach(() => {
+  const chainId = ChainId.MAINNET;
+  provider = new ethers.JsonRpcProvider(process.env.ETHEREUM_RPC_URL);
+
+  USDT = new Token(
+    chainId,
+    TOKENS.USDT.mainnet,
+    TOKENS.USDT.decimals,
+    "USDT",
+    "Tether USD"
+  );
+
+  WETH = new Token(
+    chainId,
+    TOKENS.WETH.mainnet,
+    TOKENS.WETH.decimals,
+    "WETH",
+    "Wrapped Ether"
+  );
+
+  BNB = new Token(
+    chainId,
+    TOKENS.BNB.mainnet,
+    TOKENS.BNB.decimals,
+    "BNB",
+    "Binance Coin"
+  );
+});
+
+describe("createPair", () => {
+  it("should return a valid pair", async () => {
+    const pair = await createPair(WETH, USDT, provider);
+
+    expect(pair).toBeDefined();
+  });
+
+  it("should create a pair with the correct token info", async () => {
+    const pair = await createPair(WETH, USDT, provider);
+    const [tokenAmount0, tokenAmount1] = pair["tokenAmounts"];
+
+    const token0 = tokenAmount0["currency"];
+    const token1 = tokenAmount1["currency"];
+
+    expect(token0["symbol"]).toBe("WETH");
+    expect(token0["decimals"]).toBe(18);
+    expect(token0["address"]).toBe(TOKENS.WETH.mainnet);
+    expect(token1["symbol"]).toBe("USDT");
+    expect(token1["decimals"]).toBe(6);
+    expect(token1["address"]).toBe(TOKENS.USDT.mainnet);
+  });
+});
+
+describe("getMidPrice", () => {
+  it("should return WETH token price in USDT", async () => {
+    const midPrice = await getMidPrice(WETH, USDT, provider);
+    expect(midPrice).toBeCloseTo(3791.65, -1);
+  });
+});
+
+describe("getExecutionPrice", () => {
+  it("should return execution price of WETH token in USDT", async () => {
+    const executionPrice = await getExecutionPrice(WETH, USDT, provider, 1);
+    expect(executionPrice).toBeCloseTo(3782.01, -1);
+  });
+});
