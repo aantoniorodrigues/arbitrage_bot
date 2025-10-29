65bfc7425a818cfcbdb73027a4c59be77af780bf
Antonio Rodrigues <antoniocvpr@gmail.com>
2025-10-29 16:32:10 +0000

    create token pairs no sushiswap

---
+++ listener/src/dexs/sushiswap/sushiswap.test.ts
@@ -0,0 +1,82 @@
+import { TOKENS } from "../../constants";
+import { ChainId, Token } from "@uniswap/sdk-core";
+import { createPair } from "./sushiswap";
+import { ethers } from "ethers";
+
+let USDT: Token;
+let WETH: Token;
+let BNB: Token;
+let provider: ethers.JsonRpcProvider;
+
+beforeEach(() => {
+  const chainId = ChainId.MAINNET;
+  provider = new ethers.JsonRpcProvider(process.env.ETHEREUM_RPC_URL);
+
+  USDT = new Token(
+    chainId,
+    TOKENS.USDT.mainnet,
+    TOKENS.USDT.decimals,
+    "USDT",
+    "Tether USD"
+  );
+
+  WETH = new Token(
+    chainId,
+    TOKENS.WETH.mainnet,
+    TOKENS.WETH.decimals,
+    "WETH",
+    "Wrapped Ether"
+  );
+
+  BNB = new Token(
+    chainId,
+    TOKENS.BNB.mainnet,
+    TOKENS.BNB.decimals,
+    "BNB",
+    "Binance Coin"
+  );
+});
+
+describe("createPair", () => {
+  it("should return a valid pair", async () => {
+    const pair = await createPair(WETH, USDT, provider);
+
+    expect(pair).toBeDefined();
+  });
+
+  it("should create WETH/USDT pair with the correct token info", async () => {
+    const pair = await createPair(WETH, USDT, provider);
+    const [tokenAmount0, tokenAmount1] = pair["tokenAmounts"];
+
+    const token0 = tokenAmount0["currency"];
+    const token1 = tokenAmount1["currency"];
+    console.log("token0", token0);
+    console.log("token1", token1);
+
+    expect(token0["symbol"]).toBe("WETH");
+    expect(token0["decimals"]).toBe(18);
+    expect(token0["address"]).toBe(TOKENS.WETH.mainnet);
+    expect(token1["symbol"]).toBe("USDT");
+    expect(token1["decimals"]).toBe(6);
+    expect(token1["address"]).toBe(TOKENS.USDT.mainnet);
+  });
+
+
+  it("should create WETH/BNB pair with the correct token info", async () => {
+    const pair = await createPair(WETH, BNB, provider);
+    console.log("tokenAmounts", pair["tokenAmounts"]);
+    const [tokenAmount0, tokenAmount1] = pair["tokenAmounts"];
+
+    const token0 = tokenAmount0["currency"];
+    const token1 = tokenAmount1["currency"];
+    console.log("token0", token0);
+    console.log("token1", token1);
+
+    expect(token0["symbol"]).toBe("BNB");
+    expect(token0["decimals"]).toBe(18);
+    expect(token0["address"]).toBe(TOKENS.BNB.mainnet);
+    expect(token1["symbol"]).toBe("WETH");
+    expect(token1["decimals"]).toBe(18);
+    expect(token1["address"]).toBe(TOKENS.WETH.mainnet);
+  });
+});
