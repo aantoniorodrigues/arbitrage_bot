a929ac575de2e6068e02beebfc7388ca72d950f6
Antonio Rodrigues <antoniocvpr@gmail.com>
2026-02-26 15:02:42 +0800

    define DEXs objects for each DEX

--- listener/src/dexs/sushiswap/abi.ts
+++
@@ -1,3 +0,0 @@
-export const ABI = [
-    "function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)"
-  ];
--- listener/src/dexs/uniswap/uniswap.ts
+++ listener/src/dexs/uniswap/uniswap.ts
@@ -1,66 +1,16 @@
-import { Token, CurrencyAmount, TradeType } from "@uniswap/sdk-core";
-import { Pair, Route, Trade } from "@uniswap/v2-sdk";
-import { ethers } from "ethers";
-import uniswapV2poolABI from "@uniswap/v2-core/build/UniswapV2Pair.json";
+import { Dex } from "../../interfaces/types";
+import { ABI } from "../abi";
 
-export async function createPair(
-  token0: Token,
-  token1: Token,
-  provider: ethers.JsonRpcProvider
-): Promise<Pair> {
-  const pairAddress = Pair.getAddress(token0, token1);
-  const pairContract = new ethers.Contract(
-    pairAddress,
-    uniswapV2poolABI.abi,
-    provider
-  );
+export class Uniswap implements Dex {
+  name: string;
+  abi: string[];
+  factoryAddress: string;
+  initCodeHash: string;
 
-  const reserves = await pairContract["getReserves"]();
-  const [reserve0, reserve1] = reserves;
-
-  const tokens = [token0, token1];
-  const tokensSorted = tokens[0].sortsBefore(tokens[1])
-    ? tokens
-    : [tokens[1], tokens[0]];
-
-  const reservesSorted = token0.sortsBefore(token1)
-    ? reserves
-    : [reserve1, reserve0];
-
-  const pair = new Pair(
-    CurrencyAmount.fromRawAmount(tokensSorted[0], reservesSorted[0].toString()),
-    CurrencyAmount.fromRawAmount(tokensSorted[1], reservesSorted[1].toString())
-  );
-
-  return pair;
-}
-
-export async function getMidPrice(
-  inputToken: Token,
-  outputToken: Token,
-  provider: ethers.JsonRpcProvider
-): Promise<number> {
-  const pair = await createPair(inputToken, outputToken, provider);
-  const route = new Route([pair], inputToken, outputToken);
-
-  return Number(route.midPrice.toSignificant(6));
-}
-
-export async function getExecutionPrice(
-  inputToken: Token,
-  outputToken: Token,
-  provider: ethers.JsonRpcProvider,
-  inputTokenAmount: number
-): Promise<number> {
-const pair = await createPair(inputToken, outputToken, provider)
-const route = new Route([pair], inputToken, outputToken);
-const amountWithDecimals = inputTokenAmount * 10 ** inputToken.decimals;
-
-const trade = new Trade(
-  route,
-  CurrencyAmount.fromRawAmount(inputToken, amountWithDecimals),
-  TradeType.EXACT_INPUT
-);
-
-return Number(trade.executionPrice.toSignificant(6));
+  constructor() {
+    this.name = "Uniswap";
+    this.abi = ABI;
+    this.factoryAddress = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+    this.initCodeHash = "0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f";
+  }
 }
