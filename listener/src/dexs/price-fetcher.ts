db5ac10a8abe884a392cf8b2648698d68cd87b71
Antonio Rodrigues <antoniocvpr@gmail.com>
2026-02-26 14:58:17 +0800

    abstract price fetching logic

---
+++ listener/src/dexs/price-fetcher.ts
@@ -0,0 +1,85 @@
+import { Token, CurrencyAmount, TradeType } from "@uniswap/sdk-core";
+import { Pair, Route, Trade } from "@uniswap/v2-sdk";
+import { ethers } from "ethers";
+import { Dex } from "../interfaces/types";
+
+export async function createPair(
+  token0: Token,
+  token1: Token,
+  provider: ethers.JsonRpcProvider,
+  dex: Dex,
+): Promise<Pair> {
+  const pairAddress = getPairAddress(token0, token1, dex);
+  const pairContract = new ethers.Contract(pairAddress, dex.abi, provider);
+
+  const reserves = await pairContract["getReserves"]();
+  const [reserve0, reserve1] = reserves;
+
+  const tokens = [token0, token1];
+  const tokensSorted = tokens[0].sortsBefore(tokens[1])
+    ? tokens
+    : [tokens[1], tokens[0]];
+
+  const reservesSorted = token0.sortsBefore(token1)
+    ? reserves
+    : [reserve1, reserve0];
+
+  const pair = new Pair(
+    CurrencyAmount.fromRawAmount(tokensSorted[0], reservesSorted[0].toString()),
+    CurrencyAmount.fromRawAmount(tokensSorted[1], reservesSorted[1].toString()),
+  );
+
+  return pair;
+}
+
+export async function getMidPrice(
+  inputToken: Token,
+  outputToken: Token,
+  provider: ethers.JsonRpcProvider,
+  dex: Dex,
+): Promise<number> {
+  const pair = await createPair(inputToken, outputToken, provider, dex);
+  const route = new Route([pair], inputToken, outputToken);
+
+  return Number(route.midPrice.toSignificant(6));
+}
+
+export async function getExecutionPrice(
+  inputToken: Token,
+  outputToken: Token,
+  provider: ethers.JsonRpcProvider,
+  inputTokenAmount: number,
+  dex: Dex,
+): Promise<number> {
+  const pair = await createPair(inputToken, outputToken, provider, dex);
+  const route = new Route([pair], inputToken, outputToken);
+  const amountWithDecimals = inputTokenAmount * 10 ** inputToken.decimals;
+
+  const trade = new Trade(
+    route,
+    CurrencyAmount.fromRawAmount(inputToken, amountWithDecimals),
+    TradeType.EXACT_INPUT,
+  );
+
+  return Number(trade.executionPrice.toSignificant(6));
+}
+
+function getPairAddress(tokenA: Token, tokenB: Token, dex: Dex): string {
+  const [token0, token1] = tokenA.sortsBefore(tokenB)
+    ? [tokenA, tokenB]
+    : [tokenB, tokenA];
+
+  return ethers.getCreate2Address(
+    dex.factoryAddress,
+    ethers.solidityPackedKeccak256(
+      ["bytes"],
+      [
+        ethers.solidityPacked(
+          ["address", "address"],
+          [token0.address, token1.address],
+        ),
+      ],
+    ),
+    dex.initCodeHash,
+  );
+}
