c4f79413f5cbe08aea9b60894d34928eaefbfbdf
Antonio Rodrigues <antoniocvpr@gmail.com>
2026-06-25 17:07:36 +0800

    format codebase

--- listener/src/dexs/sushiswap/sushiswap.ts
+++ listener/src/dexs/sushiswap/sushiswap.ts
@@ -11,6 +11,7 @@ export class Sushiswap implements Dex {
     this.name = "Sushiswap";
     this.abi = ABI;
     this.factoryAddress = "0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac";
-    this.initCodeHash = "0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303";
+    this.initCodeHash =
+      "0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303";
   }
 }
